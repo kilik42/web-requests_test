a7b710be89ae9dcfdca329c2fe554d4409981451
marvin evins <maevins@cps.edu>
2016-06-06 11:23:40 -0500

    NSURLSession
    NSURLSession testing again

--- web-requests_test/ViewController.swift
+++ web-requests_test/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "http://swapi.com/api/people/1/"
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: urlString)!
+        
+        session.dataTaskWithURL(url) { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            if let responseData = data {
+                do{
+                  let json =  try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments)
+                    print(json)
+                } catch{
+                    
+                    print("could not serialize")
+                    
+                }
+                
+                
+            }
+        }.resume()
     }
 
     override func didReceiveMemoryWarning() {
