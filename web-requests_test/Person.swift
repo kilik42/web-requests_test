042dc9ba39cac94fad66740c37fdca9d9916efac
marvin evins <maevins@cps.edu>
2016-06-06 18:10:44 -0500

    web requests
    web requests with son

---
+++ web-requests_test/Person.swift
@@ -0,0 +1,40 @@
+//
+//  Person.swift
+//  web-requests_test
+//
+//  Created by marvin evins on 6/6/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+import Foundation
+
+class SWPerson {
+    private var _name:  String!
+    private var _height: String!
+    private var _birthYear: String!
+    private var _hairColor: String!
+    
+    var name: String{
+        return _name
+    }
+    var height: String{
+        get{
+            return _height
+        }
+    }
+    var birthYear: String{
+        return _birthYear
+    }
+    var hairColor:  String{
+        return _hairColor
+    }
+    
+    init(name: String, height: String, birthYear: String, hairColor: String){
+        _name = name
+        _height = height
+        _birthYear = birthYear
+        _hairColor = hairColor
+    }
+    
+    
+}
--- web-requests_test/ViewController.swift
+++ web-requests_test/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let urlString = "http://swapi.com/api/people/1/"
+        let urlString = "http://swapi.com/api/planets/1/"
         let session = NSURLSession.sharedSession()
         let url = NSURL(string: urlString)!
         
@@ -22,7 +25,26 @@ class ViewController: UIViewController {
             if let responseData = data {
                 do{
                   let json =  try NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.AllowFragments)
-                    print(json)
+                   
+                    if let dict = json as? Dictionary<String, AnyObject>{
+                        
+                       // print("Did we get here: \(dict.debugDescription)")
+                        if let name = dict["name"] as? String, let height = dict["height"] as? String, let birth = dict["birth_year"] as? String, let hair = dict["hair_color"] as? String{
+                            let person = SWPerson(name: name , height: height, birthYear: birth, hairColor: hair)
+                           print(person.name)
+                           print(person.height)
+                           print(person.hairColor)
+                           print(person.birthYear)
+                            
+                            if let films = dict["films"] as? [String]{
+                                for film in films {
+                                    print(film)
+                                }
+                            }
+                        }
+                    }
+                    
+                    
                 } catch{
                     
                     print("could not serialize")
